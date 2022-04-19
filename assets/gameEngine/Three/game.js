f495909ea2eaddb334fb9a42b96af0038221da3e
iamOdde <odde.ahlgren@gmail.com>
2022-04-19 13:42:21 +0200

    three js

---
+++ assets/gameEngine/Three/game.js
@@ -0,0 +1,45 @@
+import * as THREE from "../../../node_modules/three/build/three.module.js";
+
+import { OrbitControls } from "../../../node_modules/three/examples/jsm/controls/OrbitControls.js";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(800, 400);
+
+camera.position.setZ(30);
+
+const geometry = new THREE.SphereGeometry(15, 32, 16);
+const material = new THREE.MeshStandardMaterial({ color: 0xffff00 });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.005;
+  sphere.rotation.z += 0.01;
+
+  //controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
