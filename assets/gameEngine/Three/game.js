61e9368a78235760ee921dc9c4dee22423b01c4f
iamOdde <odde.ahlgren@gmail.com>
2022-04-20 09:41:50 +0200

    three js orbit

--- assets/gameEngine/Three/game.js
+++ assets/gameEngine/Three/game.js
@@ -20,26 +20,50 @@ renderer.setSize(800, 400);
 
 camera.position.setZ(30);
 
-const geometry = new THREE.SphereGeometry(15, 32, 16);
-const material = new THREE.MeshStandardMaterial({ color: 0xffff00 });
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+const pointerLight = new THREE.PointLight(0xffffff);
+pointerLight.position.set(20, 20, 20);
 
-const light = new THREE.AmbientLight(0xffffff);
-scene.add(light);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointerLight, ambientLight);
 
-//const controls = new OrbitControls(camera, renderer.domElement);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.005;
-  sphere.rotation.z += 0.01;
+  smallSphere.rotation.x += 0.01;
+  smallSphere.rotation.y += 0.005;
+  smallSphere.rotation.z += 0.01;
 
-  //controls.update();
+  controls.update();
 
   renderer.render(scene, camera);
 }
+
+//Skapa första bollen
+const smallSphereTemp = new THREE.SphereGeometry(0.9, 32, 16);
+const smallSphereColor = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const smallSphere = new THREE.Mesh(smallSphereTemp, smallSphereColor);
+
+smallSphere.position.y = 10;
+
+scene.add(smallSphere);
+
+//Storlekar
+const mapWidth = 25;
+const mapLenght = 2;
+const mapHight = 50;
+
+// Skapa Grunden
+const groundTemp = new THREE.BoxGeometry(mapWidth, 1, mapLenght);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  side: THREE.DoubleSide,
+});
+const ground = new THREE.Mesh(groundTemp, material);
+scene.add(ground);
 
 animate();
